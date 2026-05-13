483f942fe4c4ad6d5b7ab8a837f7314199d038c3
andre <aramayotorrezandre@pedropoveda.edu.bo>
2026-05-13 12:27:28 -0400

    form y registro
    dificil (andre)

--- registro.php
+++ registro.php
@@ -1,16 +1,21 @@
 <?php
-$direccion = "localhost";
-$usuario = "root";
-$password = "";
-$baseDatos = "produccion";
-$conexion = new mysqli($direccion,$usuario,$password,$baseDatos);
+$ciu= "localhost";
+$nombreu= "root";
+$direccion= "root";
+$celular= "root";
+$rol= "root";
+$estado= "root";
+$nombrebase= "dragonice";
+$conexion= new mysqli($ciu,$nombreu,$direccion,$celular,$rol,$estado,$nombrebase);
 if($conexion->error) {
     echo" Hubo un error al conectar a la base de datos";
 }
-$nombre =$_POST['nombre'];
-$apellido=$_POST['apellido'];
-$descripcion=$_POST['descripcion'];
-$sql ="INSERT INTO personas(nombre, apellido, descripcion) VALUES ('$nombre', '$apellido', '$descripcion')";
+$nombreu=$_POST['nombre'];
+$direccion=$_POST['direccion'];
+$celular=$_POST['celular'];
+$rol=$_POST['rol'];
+$estado=$_POST['estado'];
+$sql ="INSERT INTO personas(nombreu, direccion, celular, rol, estado) VALUES ('$nombreu', '$direccion', '$celular', '$rol', '$estado')";
 if($conexion->query($sql)===TRUE){
     echo "Registro exitoso";
 }
