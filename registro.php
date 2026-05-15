8c8fd830d731dc0cacb68e77844da39ca968f5dc
andre <aramayotorrezandre@pedropoveda.edu.bo>
2026-05-14 23:11:44 -0400

    crud
    andre

--- registro.php
+++ registro.php
@@ -1,21 +1,21 @@
 <?php
 $ciu= "localhost";
-$nombreu= "root";
+$nombre= "root";
 $direccion= "root";
 $celular= "root";
 $rol= "root";
 $estado= "root";
-$nombrebase= "dragonice";
-$conexion= new mysqli($ciu,$nombreu,$direccion,$celular,$rol,$estado,$nombrebase);
+$nombrebase= "dragon";
+$conexion= new mysqli($ciu,$nombre,$direccion,$celular,$rol,$estado,$nombrebase);
 if($conexion->error) {
     echo" Hubo un error al conectar a la base de datos";
 }
-$nombreu=$_POST['nombre'];
+$nombre=$_POST['nombre'];
 $direccion=$_POST['direccion'];
 $celular=$_POST['celular'];
 $rol=$_POST['rol'];
 $estado=$_POST['estado'];
-$sql ="INSERT INTO personas(nombreu, direccion, celular, rol, estado) VALUES ('$nombreu', '$direccion', '$celular', '$rol', '$estado')";
+$sql ="INSERT INTO personas(nombre,direccion, elular,rol,estado) VALUES ('$nombre', '$direccion', '$celular', '$rol', '$estado')";
 if($conexion->query($sql)===TRUE){
     echo "Registro exitoso";
 }
