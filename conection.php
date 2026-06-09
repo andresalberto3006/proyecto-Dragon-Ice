858fbd303c58ee48ed4112551fbe6b3708f2bc23
varguiñas1119 <vargascamila306@gmail.com>
2026-06-09 14:05:46 -0400

    camila

--- conection.php
+++ conection.php
@@ -3,12 +3,13 @@ function connection(){
     $host = "localhost";
     $user = "root";
     $pass = "";
-
     $bd = "dragonice";
 
-    $connect=mysqli_connect($ciu, $nombre, $direccion, $celular, $rol, $estado);
+    $connect=new mysqli($ciu, $nombre, $direccion, $celular, $rol, $estado);
 
-    mysqli_select_db($connect, $bd);
+    if ($conn->connect_error) {
+die("Conexión fallida: " . $conn->connect_error);
+}
 
     return $connect;
 
--- read2producto.php
+++ read2producto.php
@@ -5,11 +5,17 @@ $contraseña="";
 $nombreBase="dragonice";
 
 $conexion= new mysqli($direccion,$usuario,$contraseña,$nombreBase);
-if ($conn->connect_error){
-    die("Error de conexion");
+if($conexion->error){
+    echo "hubo un error al conectar con la base de datos";
 }
-$sql = "SELECT * FROM producto"
-$result = $conn->query($sql);
+$id=$_GET['id']
+$sql="SELECT *FROM personas WHERE id="$id";
+$resultado = $conexion->query($sql);
+if ($resultado->num_rows>0){
+while($fila=$resultado->fetch_assoc()){
+echo $fila['codigo']."<br>".$fila['nombre']."<br>".$fila['descripcion']."<br>".$fila['precio']."<br>".$fila['costo']."<br>".$fila['stock'];
+  }
+}
 ?>
 <!DOCTYPE html>
 <html lang="es">
