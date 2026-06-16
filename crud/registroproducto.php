ad6786b9065cd1b448345113d2d9b8c83d0f621a
edson torrico <torricoedson7@gmail.com>
2026-06-16 12:26:46 -0400

    edson hizo algo

--- crud/registroproducto.php
+++ crud/registroproducto.php
@@ -4,17 +4,17 @@
   $contraseña="";
   $nombreBase="dragonice";
 
-  $connect=new mysqli($direccion, $usuario, $contraseña, $nombreBase);
+  $conexion=new mysqli($direccion, $usuario, $contraseña, $nombreBase);
     if ($conn->connect_error) {
         echo "hubo un error al conectar a las base de datos";
 }
-$Codigo=$_POST['Codigo'];
+$Codigo=$_POST['ci'];
 $nombre=$_POST['nombre'];
-$descripcin=$_POST['direccion'];
-$precio=$_POST['celular'];
-$costo=$_POST['rol'];
-$stock=$_POST['estado'];
-$sql ="INSERT INTO producto(Codigo, nombre, descripcion, precio, costo, stock) VALUES ('$id', '$nombre', '$descripcion', '$precio', '$costo', '$stock')";
+$descripcin=$_POST['descripcion'];
+$precio=$_POST['precio'];
+$costo=$_POST['costo'];
+$stock=$_POST['stock'];
+$sql ="INSERT INTO producto(ci, nombre, descripcion, precio, costo, stock) VALUES ('$Codigo', '$nombre', '$descripcin', '$precio', '$costo', '$stock')";
 if ($conexion->query($sql)==TRUE){
   echo "Se registro correctamente";
 }
