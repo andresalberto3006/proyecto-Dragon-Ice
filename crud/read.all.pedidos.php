e7bc36b90ef0bf9ae572c5426c9e60c07d58c6fc
1111cam <vargascamila306@gmail.com>
2026-06-19 12:13:19 -0400

    Revert "crud"
    This reverts commit d0a60542ef649371ecd99d7cb58a6518c653abc9.

---
+++ crud/read.all.pedidos.php
@@ -0,0 +1,22 @@
+<?php
+$direccion="localhost";
+$usuario="root";
+$contrasena="";
+$nombreBase="dragonice";
+
+$conexion= new mysqli($direccion,$usuario,$contrasena,$nombreBase);
+if($conexion->error){
+echo "Hubo un error al conectar a la base de datos";
+}
+
+$sql= "SELECT * FROM personas";
+$resultado = $conexion->query($sql);
+if ($resultado->num_rows>0){
+while($fila-$resultado->fetch_assoc()){
+echo $fila['id']."<br>".$fila['nombre']."<br>".$fila['fecha']."<br>".$fila['estado']."<br>".$fila['nombrevendedor']."<br>";
+$idPersona-$fila['id'];
+echo "<a href=readpedidos.php?id=$idpedidos'><button >Mostrar</button></a><br>";
+echo "<a href=updatepedidos.php?id=$idpedidos'><button >Mostrar</button></a><br>";
+}
+}
+?>
---
+++ crud/registrousuario.php
@@ -0,0 +1,27 @@
+<?php
+  $direccion="localhost";
+  $usuario="root";
+  $contraseña="";
+  $nombreBase="dragonice";
+
+  $conexion=new mysqli($direccion, $usuario, $contraseña, $nombreBase);
+    if ($conexion->error) {
+        echo "Hubo un error al conectar a la base de datos";
+}
+
+$ci=$_POST['ci'];
+$nombre=$_POST['nombre'];
+$direccion=$_POST['direccion'];
+$celular=$_POST['celular'];
+$rol=$_POST['rol'];
+$estado=$_POST['estado'];
+$sql ="INSERT INTO usuario(ci, nombre, direccion, celular, rol, estado) VALUES ('$ci','$nombre', '$direccion', '$celular', '$rol', '$estado')";
+if ($conexion->query($sql)===TRUE){
+  echo "Se registro correctamente";
+  header("Location: readusuario.php?ci=$ci");
+}
+else{
+  echo $sql->error;
+}
+
+?>
