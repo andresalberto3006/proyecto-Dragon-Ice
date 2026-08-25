1edec44c1f82ae82566b63a09d58714b422b477d
aramayotorrezandre-dev <aramayotorrezandre@pedropoveda.edu.bo>
2026-08-25 14:29:16 -0400

    commit
    agregue la carpeta de comentarios

---
+++ comentarios/formulario.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="mensaje.php" method="POST">
+        <label for="">Asunto:</label>
+        <input type="text" name="asunto" id="">
+        <label for="">Comentario:</label>
+        <textarea name="come" id=""></textarea>
+        <input type="submit" value="ENVIAR">
+        <input type="reset" value="BORRAR">
+    </form>
+</body>
+</html>               
