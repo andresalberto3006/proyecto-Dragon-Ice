31a3d93244d712fe6894db27862eac5df7cd2156
torricoedson7-debug <torricoedson7@gmail.com>
2026-09-04 11:14:59 -0400

    ingresostotal

--- paginaprincipal/02.admin.php
+++ paginaprincipal/02.admin.php
@@ -156,7 +156,7 @@ $rutaMenu = "../";
             <div class="cuerpo">
                 <h2>Ventas generales</h2>
                 <p>Muestra todas las ventas registradas en el sistema. El administrador puede revisar, editar o eliminar ventas.</p>
-                <a href="../ventas/ventas.php" class="boton">Ver ventas</a>
+                <a href="../reportes/ingre-total.php" class="boton">Ver ventas</a>
             </div>
         </article>
 
